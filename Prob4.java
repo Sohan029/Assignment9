06fae05c3e840fdcda0d5dedf224ce475387f04e
Sohan029 <121715317+Sohan029@users.noreply.github.com>
2023-03-07 17:36:53 +0530

    Prob4.java

---
+++ Prob4.java
@@ -0,0 +1,26 @@
+package Assignment9;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Prob4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+
+//        int min=arr[0];
+//        for(int i=0;i<n;i++){
+//           if(arr[i]<min){
+//               min=arr[i];
+//           }
+//        }
+        int min = Arrays.stream(arr).min().getAsInt();
+        System.out.println(min);
+
+    }
+}
